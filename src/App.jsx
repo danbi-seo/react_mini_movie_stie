6f5f86afcfe707aebcfd847cc467242c67013867
danbi-seo <sdb0504@naver.com>
2025-07-25 13:14:14 +0900

    BrowserRouter 위치를 main.jsx로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import MovieDetail from './components/MovieDetail';
 import movieDetailData from './assets/data/movieDetailData.json';
 import movieListData from './assets/data/movieListData.json';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import Layout from './components/Layout';
 
@@ -12,14 +12,14 @@ function App() {
   console.log("movies array:", movies);
     console.log("movieDetailData loaded in App.jsx:", movieDetailData);
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<HomePage />} />
           <Route path='/movie/:id' element={<MovieDetail movie={movieDetailData}/>} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
