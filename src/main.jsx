7a07bf06ae8c2b3f82313710619580e03cfa7ac2
danbi-seo <sdb0504@naver.com>
2025-08-19 00:28:12 +0900

    HashRouter로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { SupabaseProvider } from "./supabase/context/index.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  // <BrowserRouter>
+  <HashRouter>
     <SupabaseProvider>
       <App />
     </SupabaseProvider>
-  </BrowserRouter>
+  </HashRouter>
+  //</BrowserRouter>
 );
 
 // 로그인 버튼을 눌렀을때 신호가 안넘어가짐
